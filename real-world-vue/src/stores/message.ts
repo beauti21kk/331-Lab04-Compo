3a23112e976186050f5176fa41a03305b059fcac
Beautiiiii <95921996+Beautiiiii@users.noreply.github.com>
2023-08-05 19:00:44 +0700

    1.3

--- real-world-vue/src/stores/message.ts
+++ real-world-vue/src/stores/message.ts
@@ -2,5 +2,13 @@ import { defineStore } from 'pinia'
 export const useMessageStore = defineStore('message', {
     state: () => ({
         message: '' as string
-    })
+    }),
+    actions: {
+        updateMessage(message: string) {
+            this.message = message
+        },
+        resetMessage() {
+            this.message = ''
+        }
+    }
 }) 
