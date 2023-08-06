fc8c83d1049d0c2db2156b214994f8652618b745
Beautiiiii <95921996+Beautiiiii@users.noreply.github.com>
2023-08-06 14:44:20 +0700

    1.6

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from '@/stores/message';
+import { storeToRefs } from 'pinia';
+const store = useMessageStore()
+const { message } = storeToRefs(store)
 import {ref} from 'vue'
 const sizes = ref<number>(2)
 import HelloWorld from './components/HelloWorld.vue'
@@ -17,6 +21,9 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink :to="{name: 'about'}">About</RouterLink>
         <RouterLink :to="{name: 'category'}">Category</RouterLink>
         <RouterLink :to="{name: 'student'}">Student</RouterLink>
+        <div id="flashMessage" v-if="message">
+          <h4>{{ message }}</h4>
+        </div>
       </nav>
       <div class="size">
         <label>  Size : </label>
@@ -92,5 +99,17 @@ nav a:first-of-type {
 }
 h4 {
   font-size: 20px;
+}
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation: yellowfade 3s ease-in-out;
 }
 </style>
