cb07462aeebaa17707fffa578697d3d400674961
Beautiiiii <95921996+Beautiiiii@users.noreply.github.com>
2023-08-08 02:43:20 +0700

    3

--- real-world-vue/src/views/event/EventLayoutView.vue
+++ real-world-vue/src/views/event/EventLayoutView.vue
@@ -1,28 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { type EventItem } from '@/type'
-import EventService from '@/services/EventService'
 import { useRouter } from 'vue-router';
-const event = ref<EventItem | null>(null)
-
-const props = defineProps({
-    id: String
-})
+import { useEventStore } from '@/stores/event'
+import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
 
 const router = useRouter()
-EventService.getEventById(Number(props.id))
-.then((response) => {
-    event.value = response.data
-})
-.catch((error) => {
-    console.log(error)
-    if(error.response && error.response.status === 404) {
-        router.push({ name: '404-resource', params: { resource: 'event'} })
-    }
-    else {
-        router.push({ name: 'network-error' })
-    }
-})
+const store = useEventStore()
+const event = storeToRefs(store).event
+const id = ref(event?.value?.id)
+
 </script>
 
 <template>
